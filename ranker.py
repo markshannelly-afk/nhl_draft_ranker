54215fd8db0e25b5a18ed383e80b73fe5157fcfc
Shanster925 <markshannelly@gmail.com>
2026-07-16 23:51:28 -0400

    Update ranker.py
    Adjusted weighting for previous season stats.

--- ranker.py
+++ ranker.py
@@ -174,9 +174,9 @@ AGE_CONFIG = config.get(
 # Helps if a player had a million points when they were 14.
 SEASON_WEIGHTS = {
     "2025": 1.00,
-    "2024": 0.75,
-    "2023": 0.50,
-    "2022": 0.25
+    "2024": 0.5,
+    "2023": 0.35,
+    "2022": 0.15
 }
 
 
